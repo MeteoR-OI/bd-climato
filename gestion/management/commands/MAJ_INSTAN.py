bcb1282c2134b69a6960724e4f07ae15273c7ccc
Keryl Clain <kerylclain974@hotmail.fr>
2019-04-16 21:04:18 +0200

    MAJ INSTAN initial

--- gestion/management/commands/MAJ_INSTAN.py
+++ gestion/management/commands/MAJ_INSTAN.py
@@ -11,9 +11,9 @@ from gestion.models import POSTE,INSTAN
 
 # si la valeur n'existe pas --> none 
 def convert(value):
- 
+   
     try:
-        value= float(value)
+        return float(value)
     except:
         return None 
         
@@ -47,45 +47,24 @@ class Command(BaseCommand):
                        
                     data = datas['stats']
                     data = data['current']
-                    outTemp = data['outTemp']
-                    windchill = data['windchill']
-                    heatIndex = data['heatIndex']
-                    dewpoint = data['dewpoint']
-                    humidity = data['humidity']
-                    barometer = data['barometer']
-                    windSpeed = data['windSpeed']
-                    windDir = data['windDir']
-                    windGust = data['windGust']
-                    windGustDir = ['windGustDir']
-                    rainRate = data['rainRate']
-                    rain = data['rainSum']
-                    try:
-                        ET = convert(data['ET'])
-                    except: 
-                        ET = None
-                    try : 
-                        solarRadiation = convert(data['solarRadiation'])
+                    outTemp = data['outTemp'].replace('"','').replace(',','.')
+                    windchill = data['windchill'].replace('"','').replace(',','.')
+                    heatIndex = data['heatIndex'].replace('"','').replace(',','.')
+                    dewpoint = data['dewpoint'].replace('"','').replace(',','.')
+                    humidity = data['humidity'].replace('"','').replace(',','.')
+                    barometer = data['barometer'].replace('"','').replace(',','.')
+                    windSpeed = data['windSpeed'].replace('"','').replace(',','.')
+                    windDir = data['windDir'].replace('"','').replace(',','.')
+                    windGust = data['windGust'].replace('"','').replace(',','.')
+                    windGustDir = data['windGustDir'].replace('"','').replace(',','.')
+                    rainRate = data['rainRate'].replace('"','').replace(',','.')
+                    rain = data['rainSum'].replace('"','').replace(',','.')
+                    try :            
+                        ET = data['ET'].replace('"','').replace(',','.')
+                        solarRadiation = data['solarRadiation'].replace('"','').replace(',','.')
                     except:
-                        solarRadiation = None 
-#                     outTemp = data['outTemp'].replace('"','').replace(',','.')
-#                     windchill = data['windchill'].replace('"','').replace(',','.')
-#                     heatIndex = data['heatIndex'].replace('"','').replace(',','.')
-#                     dewpoint = data['dewpoint'].replace('"','').replace(',','.')
-#                     humidity = data['humidity'].replace('"','').replace(',','.')
-#                     barometer = data['barometer'].replace('"','').replace(',','.')
-#                     windSpeed = data['windSpeed'].replace('"','').replace(',','.')
-#                     windDir = data['windDir'].replace('"','').replace(',','.')
-#                     windGust = data['windGust'].replace('"','').replace(',','.')
-#                     windGustDir = data['windGustDir'].replace('"','').replace(',','.')
-#                     rainRate = data['rainRate'].replace('"','').replace(',','.')
-#                     rain = data['rainSum'].replace('"','').replace(',','.')
-
-#                     try :            
-#                         ET = data['ET'].replace('"','').replace(',','.')
-#                         solarRadiation = data['solarRadiation'].replace('"','').replace(',','.')
-#                     except:
-#                         ET = None
-#                         solarRadiation = None
+                        ET = None
+                        solarRadiation = None
                     jour = datas['time'][0:2]
                     mois = datas['time'][3:5]
                     annee = datas['time'][6:10]
