4aaa7896fde5d444def88ef65f891f39593d99a5
CLAIN <kerylclain974@hotmail.fr>
2021-02-19 11:19:26 +0100

    Ajout de tests pour jsonPlus

---
+++ app/classes/tests/conftest.py
@@ -0,0 +1,100 @@
+from app.tools.jsonPlus import jsonPlus
+import pytest
+
+# On définit un json string qui sera disponible dans tous les tests
+# on aura juste à passer json_string en argument d'une fonction de test
+@pytest.fixture()
+def json_string():
+    json = """
+        {
+            "meteor" : "BBF015",
+            "info" : {
+                "blabla": "blabla"
+            },
+            "data":
+            [
+                {
+                    "current":
+                        {
+                            "dat" : "2021-02-11T13:09:30+00:00",
+                            "duration" : 300,
+                            "temp_out" : 29.5
+                        },
+                    "aggregations": [
+                        {
+                            "level" : "H",
+                            "temp_out_avg" : 32.75
+                        },
+                        {
+                            "level" : "D",
+                            "rain_rate_avg" : 1.23
+                        }
+                    ]
+                },
+                {
+                    "current" :
+                        {
+                            "dat" : "2021-02-11T13:09:40+00:00",
+                            "duration" : 300,
+                            "temp_out" : 30
+                        },
+                    "aggregations" : [
+                        {
+                            "level" : "H",
+                            "temp_out_avg" : 33
+                        },
+                        {
+                            "level" : "D",
+                            "rain_rate_avg" : 1.23
+                        }
+                    ]
+                }
+            ]
+        }"""
+        
+    return json
+
+#On définit une nouvelle fixture qui utilisera la méthode loads de jsonPlus
+@pytest.fixture()
+def jp_loads(json_string):
+    jp = jsonPlus()
+    return jp.loads(json_string)
+
+#On définit une nouvelle fixture qui tentera de reconvertir jp_loads en string
+@pytest.fixture()
+def jp_dumps(jp_loads):
+    jp = jsonPlus()
+    return jp.dumps(jp_loads)
+
+
+def decomp_json(json):
+    """Fonction récursive qui décompose le json en 2 listes : une de toutes les clefs et une autre de toutes les valeurs
+    data et value seront dans le même ordre"""
+    keys,values = [],[]
+  
+    for k,v in json.items():
+        if isinstance(v,dict):
+            iter_key,iter_val = decomp_json(v)
+            keys+=iter_key
+            values+=iter_val
+        elif isinstance(v,list):
+            for v_decomp in v:
+                iter_key,iter_val = decomp_json(v_decomp)
+                keys+=iter_key
+                values+=iter_val
+        else:
+            keys+= [k]
+            values+=[v]
+  
+    return keys,values
+
+
+@pytest.fixture()
+def list_of_tuple_jploads(jp_loads):
+    '''
+    Retourne une liste de couple (data,value) chargée à partir de jp_loads
+    L'avantage est qu'on aura pas à parser récursivement le json pour chaque test
+    '''
+    keys,values = decomp_json(jp_loads)
+  
+    return list(zip(keys,values))
---
+++ app/classes/tests/test_jsonPlus.py
@@ -0,0 +1,98 @@
+from app.tools.jsonPlus import jsonPlus
+import pytest
+import datetime
+import json
+
+
+
+
+
+
+def test_date_is_datetime(jp_loads):
+    '''
+    Test qui vérifiera que les clefs censées être des datetime le sont réellement.
+    '''
+    errors = []
+    list_of_tuple_jploads = list_of_tuple_loads(jp_loads)
+    #Liste des clefs censées contenir des dates (si elle est souvent utilisée, on pourra l'ajouter en fixture)
+    list_of_dates  = ['dat','last_dat_rec','max_out_temp_time']
+
+    for tuples in list_of_tuple_jploads:
+        data,val = tuples[0],tuples[1]
+        if data in list_of_dates and not isinstance(val,datetime.datetime):
+                errors += [data]
+
+    assert len(errors) == 0
+
+def test_date_is_string(jp_dumps):
+    '''
+    Test qui vérifiera que les clefs définis dans list_of_dates ne sont pas de type autre que str.
+    '''
+    
+    errors = []
+    list_of_dates  = ['dat','last_dat_rec','max_out_temp_time']
+    #On dump grâce au module json (et pas jsonPlus)
+    json_dump = json.loads(jp_dumps)
+    list_of_tuple_json = list_of_tuple_loads(json_dump)
+    
+    for tuples in list_of_tuple_json:
+        data,val = tuples[0],tuples[1]
+        if data in list_of_dates and not isinstance(val,str):
+            errors += [data]
+
+    assert len(errors) == 0
+
+def test_compare_dump_load(jp_loads,jp_dumps):
+    '''
+    On va tester si on a pas de perte d'infos en passant d'un json --> str --> json
+    '''
+    #On load le str en json grâce à JsonPlus
+    json_loadjsonPlus = jp_loads 
+    decomp_jsonPlus = list_of_tuple_loads(json_loadjsonPlus)
+
+    #On dump le json en str grâce à JsonPlus
+    json_dumpjsonPlus = jp_dumps
+    json_loadJson = jsonPlus().loads(json_dumpjsonPlus)
+    decomp_json = list_of_tuple_loads(json_loadJson)
+   
+    
+    #On regarde l'unicité
+    #uniq = set(decomp_jsonPlus+decomp_json)
+
+    #La longueur des 2 doit être la même
+    assert len(decomp_jsonPlus) == len(decomp_json)
+
+   
+
+
+#-----------------------------------------------------Quelques utilitaires qu'on pourrait aussi tester----------------------------------
+
+def decomp_json(json):
+    """Fonction récursive qui décompose le json en 2 listes : une de toutes les clefs et une autre de toutes les valeurs
+    data et value seront dans le même ordre"""
+    keys,values = [],[]
+  
+    for k,v in json.items():
+        if isinstance(v,dict):
+            iter_key,iter_val = decomp_json(v)
+            keys+=iter_key
+            values+=iter_val
+        elif isinstance(v,list):
+            for v_decomp in v:
+                iter_key,iter_val = decomp_json(v_decomp)
+                keys+=iter_key
+                values+=iter_val
+        else:
+            keys+= [k]
+            values+=[v]
+  
+    return keys,values
+
+def list_of_tuple_loads(json):
+    '''
+    Retourne une liste de couple (data,value) chargée à partir de jp_loads
+    L'avantage est qu'on aura pas à parser récursivement le json pour chaque test
+    '''
+    keys,values = decomp_json(json)
+  
+    return list(zip(keys,values))
