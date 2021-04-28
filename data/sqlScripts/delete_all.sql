03fa0f5f5dbe67a1b781408c82597c8e462bac60
nicofr83 <nicolas@cuvillier.net>
2021-04-28 12:24:56 +0400

    sql scripts
    -count_all.sql
    - delete_all.sql

---
+++ data/sqlScripts/delete_all.sql
@@ -0,0 +1,15 @@
+delete from agg_todo;
+delete from obs;
+delete from agg_hour;
+delete from agg_day;
+delete from agg_month;
+delete from agg_year;
+delete from agg_all;
+
+delete from tmp_agg_todo;
+delete from tmp_obs;
+delete from tmp_agg_hour;
+delete from tmp_agg_day;
+delete from tmp_agg_month;
+delete from tmp_agg_year;
+delete from tmp_agg_all;
