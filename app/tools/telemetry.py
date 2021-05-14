1e0656665795b119ec05e17a1e4f62d3c92d41ca
nicofr83 <nicolas@cuvillier.net>
2021-05-14 12:54:53 +0400

    fix

--- app/tools/telemetry.py
+++ app/tools/telemetry.py
@@ -60,7 +60,7 @@ class SpanMok:
         return
 
     def record_exception(self, exc: Exception):
-        t.LogException(exc, self, 2)
+        t.LogException(exc, self, {})
         return
 
     def set_attribute(self, k: str, v):
