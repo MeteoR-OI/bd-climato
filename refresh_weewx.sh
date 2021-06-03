763e26f957693769da7e2e1a362ee17afac16b79
hubertquetelard <hubert.quetelard@gmail.com>
2021-06-03 23:16:52 +0400

    =weewx files

--- refresh_weewx.sh
+++ refresh_weewx.sh
@@ -8,8 +8,8 @@ rsync -v weewx/cheetahgenerator.py weewx@192.168.1.10:root/bin/weewx
 rsync -v weewx/extensions.py weewx@192.168.1.10:root/bin/user
 rsync -v weewx/weewx.conf weewx@192.168.1.10:root
 rsync -v weewx/weewx_dev.conf weewx@192.168.1.10:root
-rsync -v weewx/skin_bootstrap.conf weewx@192.168.1.10:root/skins/Bootstrap/skin.conf
-rsync -v weewx/skin_dev.conf weewx@192.168.1.10:root/skins/Dev/skin.conf
+rsync -v weewx/skin_Bootstrap.conf weewx@192.168.1.10:root/skins/Bootstrap/skin.conf
+rsync -v weewx/skin_Dev.conf weewx@192.168.1.10:root/skins/Dev/skin.conf
 rsync -v weewx/obs.YYYY-MM-DD-HH-mm.json.tmpl weewx@192.168.1.10:root/skins/Bootstrap/json
 rsync -v weewx/dev.obs.json.tmpl weewx@192.168.1.10:root/skins/Dev/json
 
--- refresh_weewx4.sh
+++ refresh_weewx4.sh
@@ -8,7 +8,7 @@ rsync -v weewx4/cheetahgenerator.py weewx@192.168.1.10:root/bin/weewx
 rsync -v weewx4/extensions.py weewx@192.168.1.10:root/bin/user
 rsync -v weewx4/weewx.conf weewx@192.168.1.10:root
 rsync -v weewx4/weewx_dev.conf weewx@192.168.1.10:root
-rsync -v weewx4/skin_bootstrap.conf weewx@192.168.1.10:root/skins/Bootstrap/skin.conf
-rsync -v weewx4/skin_dev.conf weewx@192.168.1.10:root/skins/Dev/skin.conf
+rsync -v weewx4/skin_Bootstrap.conf weewx@192.168.1.10:root/skins/Bootstrap/skin.conf
+rsync -v weewx/skin_Dev.conf weewx@192.168.1.10:root/skins/Dev/skin.conf
 rsync -v weewx/obs.YYYY-MM-DD-HH-mm.json.tmpl weewx@192.168.1.10:root/skins/Bootstrap/json
 rsync -v weewx/dev.obs.json.tmpl weewx@192.168.1.10:root/skins/Dev/json
