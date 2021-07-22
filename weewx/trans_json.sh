d8b32f9eb853a1417765ea7d7ababd05c7ed6f93
hubertquetelard <hubert.quetelard@gmail.com>
2021-07-22 10:47:45 +0400

    =save weewx dev

--- weewx/trans_json.sh
+++ weewx/trans_json.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
- 
+
 # Save json files to json_archive :
-if mv /srv/weewx/root/public_html/json/*.json /srv/weewx/root/json_archive
-then
-echo "save files json ok"
-fi
+mv /srv/weewx/root/public_html/json/*.json /srv/weewx/root/json_archive && echo "save files json ok"
 # clear json files older than 1 days
-if find /srv/weewx/root/json_archive/*.json -mtime +1 -exec rm {} \
-echo "clear files json ok"
-fi
+find /srv/weewx/root/json_archive/*.json -mtime +7 -exec rm {} \; && echo "clear files json ok"
