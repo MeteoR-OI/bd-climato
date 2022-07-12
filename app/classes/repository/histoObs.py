efb0520c62b594daf7603239776be6041c2bc986
nicofr83 <nicolas@cuvillier.net>
2022-07-12 16:57:21 +0200

    fixes in histo writing

--- app/classes/repository/histoObs.py
+++ app/classes/repository/histoObs.py
@@ -26,15 +26,20 @@ class HistoObsMeteor():
             return
         pg_cur = pgconn.cursor()
         sql_str = "insert into histo_obs (src_obs_id, target_obs_id) values "
+        b_hasdata = False
         data.sort(key=lambda x: (x[0], x[1]))
         d0 = d1 = -1
         for an_item in data:
+            if len(an_item) != 2 or an_item[0] is None or an_item[1] is None:
+                continue
             if an_item[1] <= 0 or an_item[0] <= 0 or (d0 == an_item[0] and d1 == an_item[1]):
                 continue
             sql_str += '(' + str(an_item[0]) + ', ' + str(an_item[1]) + '), '
             d0 = an_item[0]
             d1 = an_item[1]
-        pg_cur.execute(sql_str[0:-2])
+            b_hasdata = True
+        if b_hasdata is True:
+            pg_cur.execute(sql_str[0:-2])
 
     def __str__(self):
         """print myself"""
