f7a80a3832da7264b0510ac26842cdde7a9898d7
Mickaël Hoareau <mickael.hoareau@opquast.com>
2024-05-04 18:11:29 +0400

    Reconfiguration STATIC clevercloud

--- Clim_MeteoR/settings_clevercloud.py
+++ Clim_MeteoR/settings_clevercloud.py
@@ -140,8 +140,8 @@ USE_TZ = False          # need to use no timezone !!!
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = "/srv/beta_data/meteor_oi/bd_climato/bd-climato/static"
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = "/srv/beta_data/meteor_oi/bd_climato/bd-climato/media"
