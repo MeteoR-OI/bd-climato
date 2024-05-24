891dcd7716a0c480a324d49485f7820b4e6e4272
nicofr83 <nicolas@cuvillier.net>
2024-05-24 09:04:16 +0200

    Squashed commit of the following:
    commit 0f40f54878baf1c5fe1184d9cffa11d394162f48
    Author: nicofr83 <nicolas@cuvillier.net>
    Date:   Fri May 24 09:01:05 2024 +0200
    
        symc settings et FS bucket

--- Clim_MeteoR/settings_clevercloud.py
+++ Clim_MeteoR/settings_clevercloud.py
@@ -150,11 +150,11 @@ MEDIA_ROOT = "/srv/beta_data/meteor_oi/bd_climato/bd-climato/media"
 DATA_FS_PATH = os.path.join(MEDIA_ROOT, 'data')
 
 # App settings
-JSON_AUTOLOAD = "./data/climato/json_auto_load"
-CSV_AUTOLOAD = "./data/climato/csv_auto_load"
-OVPF_FILES = "./data/climato/ovpf"
-ARCHIVE_DIR = "./data/climato/json_archive"
-FAILED_DIR = "./data/climato/erreur"
+JSON_AUTOLOAD = "./clevercloud/climato/json_auto_load"
+CSV_AUTOLOAD = "./clevercloud/climato/csv_auto_load"
+OVPF_FILES = "./clevercloud/climato/ovpf"
+ARCHIVE_DIR = "./clevercloud/climato/json_archive"
+FAILED_DIR = "./clevercloud/climato/erreur"
 
 TELEMETRY_PROVIDER = False          # None, Console, Jaeger, Thrift
 TELEMETRY_HOST = "localhost"
@@ -162,7 +162,7 @@ JAEGER_PORT = 14250
 THRIFT_PORT = 14250
 
 PROD = False
-LOG_FILE_DIR = "./data/climato/log"    # log storage
+LOG_FILE_DIR = "./clevercloud/climato/log"    # log storage
 
 # see comments in mytools.py(LogMe class definition)
 LOGGING = {
