9e43b096648d6cf4a0b2067fb48e613fd5c009d6
nicofr83 <nicolas@cuvillier.net>
2024-11-24 12:43:57 +0100

    ajout de is_maxdir dans mesures

--- app/models.py
+++ app/models.py
@@ -84,6 +84,7 @@ class Mesure(models.Model):
     min = models.BooleanField(null=True, db_default=True, verbose_name="Calcul des min")
     agreg_type = models.IntegerField(null=True, choices=Aggreg_Type.choices, db_default=Aggreg_Type.NONE, verbose_name="Type d'agregation des donnees")
     is_wind = models.BooleanField(null=True, db_default=False, verbose_name="Calcul du wind_dir")
+    is_maxdir = models.BooleanField(null=True, db_default=False, verbose_name="existance de la colonne max_dir")    
     allow_zero = models.BooleanField(null=True, db_default=True, verbose_name="Zero est une valeur valide")
     convert = models.JSONField(null=True, verbose_name="Conversion")
     j = models.JSONField(null=True, verbose_name="json data")
